7038a2018526b74dc2398bb603d42e8c35924f7d
rehm0223-lang <rehm0223@gmail.com>
2026-08-08 11:43:12 -0500

    subimos el config commit

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuración real del proyecto. Este archivo NO se sube al repositorio.
+export const firebaseConfig = {
+  apiKey: "AIzaSyBMNSjmM7sHC184DiMzzm8GmmMKTve4Ufc",
+  authDomain: "coloquio-forms.firebaseapp.com",
+  databaseURL: "https://coloquio-forms-default-rtdb.firebaseio.com",
+  projectId: "coloquio-forms",
+  storageBucket: "coloquio-forms.firebasestorage.app",
+  messagingSenderId: "170188483453",
+  appId: "1:170188483453:web:bddbe9d3ab358b70947a14",
+  measurementId: "G-70YCGNV82V",
+};
